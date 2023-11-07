6d9f5e6859c09e616c8169903338879991f9e6e7
Joshua Kaminetsky <kami@Joshuas-MacBook-Pro.local>
2023-11-06 18:54:43 -0800

    comments

--- packages/signup-wizard/src/lib/api/save-favorite.ts
+++ packages/signup-wizard/src/lib/api/save-favorite.ts
@@ -1,18 +1,14 @@
 export default async function (username: string, bookId: string) {
-    // Make sure the username and favoriteBookId are set
     if (!username || !bookId) {
         return Promise.reject('Username and favorite book must be provided');
     }
 
-    // Construct the URL with the username
     const url = `http://localhost:9000/users/${username}/favorites`;
 
-    // Construct the request payload
     const payload = {
         book: bookId
     };
 
-    // Perform the POST request to save the favorite book
     const response = await fetch(url, {
         method: 'POST',
         headers: {
